9125faa46b891fdcd2c06095d1eb58bff938b0a5
dldbdud314 <dldbdud314@naver.com>
2022-11-10 18:19:28 +0900

    [add]: StringTest

--- testcode-practice/src/test/java/study/StringTest.java
+++ testcode-practice/src/test/java/study/StringTest.java
@@ -1,7 +1,9 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -22,6 +24,23 @@ public class StringTest {
     void substringTest(){
         String str = "(1,2)";
         assertThat(str.substring(1, 4)).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("인덱스 정상 접근 테스트")
+    void charAtTest(){
+        String str = "abc";
+        assertThat(str.charAt(1)).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("인덱스 범위 밖에 접근했을 때 exception 발생")
+    void charAtExceptionTest(){
+        String str = "abc";
+        assertThatThrownBy(()->{
+            str.charAt(5);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 5");
     }
 
 }
